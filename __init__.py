8e53f68ac1f9a3df1d5f118e652e1dc15fcae448
SkyLined <github@skylined.nl>
2020-06-17 10:42:03 +0200

    Addm cHTTPHeader and cHTTPHeaders to exports

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+from .cHTTPHeader import cHTTPHeader;
+from .cHTTPHeaders import cHTTPHeaders;
 from .cHTTPProtocolException import cHTTPProtocolException;
 from .cHTTPRequest import cHTTPRequest;
 from .cHTTPResponse import cHTTPResponse;
@@ -6,6 +8,8 @@ from .iHTTPMessage import iHTTPMessage;
 from .cURL import cURL;
 
 __all__ = [
+  "cHTTPHeader",
+  "cHTTPHeaders",
   "cHTTPProtocolException",
   "cHTTPRequest",
   "cHTTPResponse",
