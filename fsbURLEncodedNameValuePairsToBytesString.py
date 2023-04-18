622fa34fb012974e083bed08c93e6d91d8806e7e
SkyLined <cojmpjum_github.com@sxd.be>
2023-04-18 23:43:07 +0200

    fAssertTypes

--- fsbURLEncodedNameValuePairsToBytesString.py
+++ fsbURLEncodedNameValuePairsToBytesString.py
@@ -1,8 +1,17 @@
 import urllib.parse;
 
+from mNotProvided import fAssertTypes;
+
 def fsbURLEncodedNameValuePairsToBytesString(dsValue_by_sName):
+  fAssertTypes({
+    "dsValue_by_sName": (dsValue_by_sName, {str: str}),
+  });
   asbData = [];
   for (sName, sValue) in dsValue_by_sName.items():
+    fAssertTypes({
+      "sName": (sName, str),
+      "sValue": (sValue, str),
+    });
     sbData = bytes(urllib.parse.quote(sName), "ascii", "strict");
     if sValue is not None:
       sbData += b"=" + bytes(urllib.parse.quote(sValue), "ascii", "strict");
