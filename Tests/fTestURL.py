50a95e375459d3d6357f3ec728ab16ac990a394b
SkyLined <cojmpjum_github.com@sxd.be>
2024-04-10 22:32:50 +0200

    Improve tests

--- Tests/fTestURL.py
+++ Tests/fTestURL.py
@@ -1,7 +1,16 @@
 from mHTTPProtocol import cURL;
+from mHTTPProtocol.cURL import grbProtocol, grbHostInURL;
 
 def fTestURL():
-  cURL.foFromBytesString(b"http://0.1.2.3:12345/path?query#hash");
-  cURL.foFromBytesString(b"https://[0:1:2:3:4:5:6:7]:12345/path?query#hash");
-  cURL.foFromBytesString(b"http://host/path?query#hash");
-  cURL.foFromBytesString(b"http://a.b-c.1.domain/path?query#hash");
+  for sbProtocol in [b"http", b"https"]:
+    assert grbProtocol.match(sbProtocol), \
+        "grbHostInURL does not match %s" % repr(sbProtocol);
+    for sbHost in [b"x", b"skylined.nl", b"1.a-z.domain", b"0.1.2.3", b"[0:1:2:3:4:5:6:7]"]:
+      assert sbHost == "" or grbHostInURL.match(sbHost), \
+          "grbHostInURL does not match %s" % repr(sbHost);
+      for sbPath in [b"", b"/", b"/path", b"/path/with/sections"]:
+        for sbQuery in [b"", b"?", b"?a", b"?a=1", b"?a=1&b=2", b"?a=1&a=2"]:
+          for sbHash in [b"", b"#", b"#hash"]:
+            sbURL = sbProtocol + b"://" + sbHost + sbPath + sbQuery + sbHash;
+            oURL = cURL.foFromBytesString(sbURL);
+            print(oURL);
